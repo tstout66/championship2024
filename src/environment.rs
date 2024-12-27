505efca979876f82d6533be5bf7e3b57d3b10535
Taylor Stout <taylor.stout@searchdiscovery.com>
2024-12-27 14:04:33 -0500

    Update teesting

---
+++ src/environment.rs
@@ -0,0 +1,25 @@
+﻿use bevy::app::{App, Plugin, Startup};
+use bevy::asset::Assets;
+use bevy::color::Color;
+use bevy::prelude::{ColorMaterial, Commands, Mesh, Mesh2d, MeshMaterial2d, Rectangle, ResMut, Transform};
+
+pub struct Environment;
+
+impl Plugin for Environment {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_entities);
+    }
+}
+
+fn spawn_entities(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let color = Color::hsl(90., 0.95, 0.7);
+    commands.spawn((
+        Mesh2d(meshes.add(Rectangle::new(50.0, 50.0))),
+        MeshMaterial2d(materials.add(color)),
+        Transform::from_xyz(200.0, 200.0, 0.0),
+    ));
+}
---
+++ src/player_camera.rs
@@ -0,0 +1,34 @@
+﻿use bevy::app::{App, Plugin, Startup};
+use bevy::prelude::{ButtonInput, Camera2d, Commands, Component, KeyCode, PostUpdate, PreUpdate, Query, Res, Single, Transform, Update, With};
+
+
+#[derive(Component)]
+pub struct PlayerCamera;
+
+impl Plugin for PlayerCamera {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Update, movement);
+    }
+
+}
+
+fn spawn_camera(
+    mut commands: Commands,
+) {
+    commands.spawn((
+        Camera2d::default(),
+        PlayerCamera,
+    ));
+}
+
+fn movement(
+    mut camera_transform: Single<&mut Transform, With<PlayerCamera>>,
+    keys: Res<ButtonInput<KeyCode>>,
+) {
+
+    if keys.pressed(KeyCode::KeyW){
+        camera_transform.translation.y += 1.0;
+        println!("{}", camera_transform.translation.y);
+    }
+}
