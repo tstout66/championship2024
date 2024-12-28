579c355022d93038a5cd9b77e9a071fe06944dba
Taylor Stout <taylor.stout@searchdiscovery.com>
2024-12-28 13:28:35 -0500

    Update teesting

--- src/gauge.rs
+++ src/gauge.rs
@@ -2,20 +2,24 @@
 use bevy::asset::Assets;
 use bevy::color::Color;
 use bevy::prelude::*;
+use noise::{NoiseFn, Perlin, Curve};
+
+pub struct GaugePlugin;
 
 #[derive(Component)]
-pub struct Gauge;
+pub struct Gauge {
+    pub curve_perlin: noise::Curve<T, Source, DIM>,
+}
 
-impl Plugin for Gauge {
+impl Plugin for GaugePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_gauge)
-        .add_systems(Startup, text_ui);
+        .add_systems(Startup, text_ui)
+        .add_systems(FixedUpdate, update_wind);
     }
 }
 
-fn text_ui(
-    mut commands: Commands,
-) {
+fn text_ui(mut commands: Commands) {
     commands.spawn((
         Text::new("Test"),
         Node {
@@ -33,10 +37,19 @@ fn spawn_gauge(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let color = Color::hsl(180., 0.95, 0.7);
+
     commands.spawn((
-        Gauge,
+        Gauge{
+            perlin: Perlin::default()
+        },
         Mesh2d(meshes.add(Rectangle::new(100.0, 100.0))),
         MeshMaterial2d(materials.add(color)),
     ));
+}
+
+fn update_wind(
+    wind_gauge_perlin: Single<&Gauge>,
+) {
+    println!("{:.32}", wind_gauge_perlin.perlin.get([3.0, 3.0, 4.0]));
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ mod player_camera;
 mod environment;
 
 use bevy::prelude::*;
-use gauge::Gauge;
+use gauge::GaugePlugin;
 use crate::player_camera::PlayerCamera;
 use crate::environment::Environment;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(Gauge)
+        .add_plugins(GaugePlugin)
         .add_plugins(PlayerCamera)
         .add_plugins(Environment)
         .run();
