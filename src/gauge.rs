c4650601864c67b529ccefd6ff1d5ec5abc96277
tstout66 <tstout66@gmail.com>
2025-01-03 13:50:19 -0500

    prototype vector lines

--- src/gauge.rs
+++ src/gauge.rs
@@ -1,27 +1,10 @@
 ﻿use std::time::Duration;
 use bevy::prelude::*;
 use perlin_noise::PerlinNoise;
-//use crate::environment::Environment;
-
-pub struct GaugePlugin;
-
-#[derive(Component)]
-pub struct Gauge {
-    pub timer: Timer,
-    pub wind_vec: Vec2,
-    pub wind_seed: Vec2,
-    pub perlin: PerlinNoise,
-}
-
-#[derive(Component)]
-pub struct Environment;
-
-#[derive(Component)]
-pub struct SpawnPoint {
-    pub pos: Vec3,
-}
 
 const MOVEMENT_FACTOR: f32 = 50.0;
+
+pub struct GaugePlugin;
 
 impl Plugin for GaugePlugin {
     fn build(&self, app: &mut App) {
@@ -30,6 +13,15 @@ impl Plugin for GaugePlugin {
         .add_systems(Update, update_wind)
         .add_systems(Update, move_with_wind);
     }
+}
+
+#[derive(Component)]
+pub struct Gauge {
+    pub pos: Vec2,
+    pub timer: Timer,
+    pub wind_vec: Vec2,
+    pub wind_seed: Vec2,
+    pub perlin: PerlinNoise,
 }
 
 fn text_ui(mut commands: Commands) {
@@ -46,25 +38,16 @@ fn text_ui(mut commands: Commands) {
 
 fn spawn_gauge(
     mut commands: Commands,
-    // mut meshes: ResMut<Assets<Mesh>>,
-    // mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let color = Color::hsl(180., 0.95, 0.7);
 
     commands.spawn((
         Gauge{
+            pos: Vec2::new(50.0, 50.0),
             timer: Timer::new(Duration::from_secs_f32(0.1), TimerMode::Repeating),
             wind_vec: Vec2::ZERO,
-            wind_seed: Vec2{x: rand::random::<f32>(), y: rand::random::<f32>()},
+            wind_seed: Vec2::new(50.0, 50.0),
             perlin: PerlinNoise::new()
-        },
-        // Mesh2d(meshes.add(Rectangle::new(100.0, 100.0))),
-        // MeshMaterial2d(materials.add(color)),
-    ));
-    commands.spawn((
-        Environment,
-        SpawnPoint {
-            pos: Vec3::new(50.,50., 50.),
         },
         Sprite {
             color,
@@ -90,6 +73,7 @@ fn update_wind(
 ) {
     gauge.timer.tick(time.delta());
     if gauge.timer.just_finished() {
+
         gauge.wind_seed.x += 0.001;
         gauge.wind_seed.y += 0.002;
         gauge.wind_vec.x = gauge.perlin.get(gauge.wind_seed.x as f64) as f32 - 0.5;
@@ -99,19 +83,19 @@ fn update_wind(
 }
 
 fn move_with_wind(
-    mut query: Query<(&SpawnPoint, &mut Transform, &mut Sprite), With<Environment>>,
+    mut query: Query<(&Gauge, &mut Transform, &mut Sprite)>,
     wind: Single<&Gauge>
 ) {
     if wind.timer.just_finished() {
-        for (spawn_point, mut transform, mut sprite) in &mut query {
+        for (gauge, mut transform, mut sprite) in &mut query {
             let extended_vector = wind.wind_vec.extend(0.0) * MOVEMENT_FACTOR;
             let length = transform.translation.distance(transform.translation + extended_vector);
             let diff = transform.translation - (transform.translation + extended_vector);
             let theta = diff.y.atan2(diff.x);
-            let midpoint = (transform.translation + (transform.translation - extended_vector)) / 2.;
+            //let midpoint = (transform.translation + (transform.translation - extended_vector)) / 2.;
 
             transform.rotation = Quat::from_rotation_z(theta);
-            transform.translation = spawn_point.pos + transform.local_x().as_vec3() * length/2.0;
+            transform.translation = gauge.pos.extend(0.0) + transform.local_x().as_vec3() * length/2.0;
             sprite.custom_size = Some(Vec2::new(length, 2.0));
         }
     }
